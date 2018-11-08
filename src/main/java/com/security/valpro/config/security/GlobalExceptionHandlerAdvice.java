003974e932d3007147fd1f01329e62cd5288c256
李建幸 <2452043851@qq.com>
2018-11-08 20:25:39 +0800

    persistense status

--- src/main/java/com/security/valpro/config/security/GlobalExceptionHandlerAdvice.java
+++ src/main/java/com/security/valpro/config/security/GlobalExceptionHandlerAdvice.java
@@ -1,4 +1,4 @@
-package com.security.valpro.controller;
+package com.security.valpro.config.security;
 
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -12,7 +12,7 @@ import java.util.List;
 public class GlobalExceptionHandlerAdvice {
     @ExceptionHandler(value = Exception.class)
     ModelAndView exception(Exception exception, WebRequest request){
-        ModelAndView modelAndView=new ModelAndView("/access-denied");
+        ModelAndView modelAndView=new ModelAndView("/error/error");
         modelAndView.addObject("errorMessage",exception.getMessage());
         modelAndView.addObject("stackTrace",exception.getStackTrace());
         return modelAndView;
--- src/main/java/com/security/valpro/service/UserService.java
+++ src/main/java/com/security/valpro/service/UserService.java
@@ -2,6 +2,9 @@ package com.security.valpro.service;
 
 import com.security.valpro.dao.UserDao;
 import com.security.valpro.entity.SysUser;
+import org.hibernate.SQLQuery;
+import org.hibernate.Session;
+import org.hibernate.transform.Transformers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -9,17 +12,28 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Predicate;
 import javax.persistence.criteria.Root;
+import java.util.List;
+import java.util.Map;
 
 @Service
 public class UserService {
     @Autowired
     private UserDao userDao;
+    @PersistenceContext
+    private EntityManager entityManager;
     public Page<SysUser> findUserCriteria(Integer page, Integer size, final SysUser user){
+
+
         Pageable pageable=new PageRequest(page,size, Sort.Direction.ASC,"id");
+
         Page<SysUser> userPage=userDao.findAll(new Specification<SysUser>() {
             @Override
             public Predicate toPredicate(Root<SysUser> root, CriteriaQuery<?> criteriaQuery, CriteriaBuilder criteriaBuilder) {
@@ -30,5 +44,36 @@ public class UserService {
             }
         },pageable);
         return userPage;
+    }
+
+    public void findTest(){
+        Session session = entityManager.unwrap(org.hibernate.Session.class);
+        String sql="";
+        session.createNativeQuery(sql).setResultSetMapping(sql);
+        //返回类型是List<map>
+		SQLQuery query2 = session.createSQLQuery(sql);
+		query2.setResultTransformer(Transformers.ALIAS_TO_ENTITY_MAP);
+		System.out.println("查询数据返回map"+query2.list());
+		//返回数据是list<List<Object>>
+		SQLQuery query3 = session.createSQLQuery(sql);
+		query2.setResultTransformer(Transformers.TO_LIST);
+		System.out.println("查询数据返回map"+query2.list());
+		//返回类型是可定制化对象
+		/*SQLQuery query = session.createSQLQuery(sql);
+		Testtrans Testtrans = new Testtrans(Test.class);
+		query.setResultTransformer(Testtrans);
+		List<Test> list = (List<Test>)query.list();
+		System.out.println("查询数据返回对象"+list);*/
+
+		Query query=entityManager.createNativeQuery(sql);
+//        List rows=query.getResultList();
+        query.unwrap(SQLQuery.class).setResultTransformer(Transformers.ALIAS_TO_ENTITY_MAP);
+        List rows = query.getResultList();
+        for (Object obj : rows) {
+            Map row = (Map) obj;
+            System.out.println("id = " + row.get("ID"));
+            System.out.println("name = " + row.get("NAME"));
+            System.out.println("age = " + row.get("AGE"));
+        }
     }
 }
