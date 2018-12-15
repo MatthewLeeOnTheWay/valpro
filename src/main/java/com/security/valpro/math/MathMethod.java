86125c1771beac859ef090fd9ec74978ee73c0bc
李建幸 <2452043851@qq.com>
2018-12-15 22:30:20 +0800

    test gitlab

---
+++ src/main/java/com/security/valpro/math/MathMethod.java
@@ -0,0 +1,18 @@
+package com.security.valpro.math;
+
+public class MathMethod {
+
+    /**
+           * 冒泡排序
+            *
+           * @param array
+           * @return
+           */
+    public static int[] bubbleSort(int[] array){
+        return null;
+    }
+
+    public static int[] selectionSort(int[] array){
+        return null;
+    }
+}
